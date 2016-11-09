ef1cf73de3d44d7c340bb52ae1995ac046a08192
FurryFur <lance.e.chaney@gmail.com>
2016-11-10 12:41:40 +1300

    Skeleton code for menu op and validation.

--- slot_machine/slot_machine/main.cpp
+++ slot_machine/slot_machine/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <random>
+#include <string>
+#include <regex>
+#include <chrono>
+#include <thread>
 
 const int g_kSLOT_MIN = 2;
 const int g_kSLOT_MAX = 7;
+const int g_kNUM_SLOTS = 3;
 
-int g_iSlot1;
-int g_iSlot2;
-int g_iSlot3;
+int g_arriSlot[g_kNUM_SLOTS];
+int g_iPlayerChips;
+
+std::random_device g_rd;     // only used once to initialise (seed) engine
+std::mt19937 g_rng(g_rd());    // random-number engine used (Mersenne-Twister in this case)
+std::uniform_int_distribution<int> g_uniR(g_kSLOT_MIN, g_kSLOT_MAX); // guaranteed unbiased
 
 int Wrap(int _iN)
 {
@@ -24,25 +32,91 @@ int Wrap(int _iN)
 
 void DrawSlotMachine()
 {
-	std::cout << "|" << Wrap(g_iSlot1 - 1) << "|" << Wrap(g_iSlot2 - 1) << "|" << Wrap(g_iSlot3 - 1) << "|" << std::endl;
-	std::cout << "|" << g_iSlot1 << "|" << g_iSlot2 << "|" << g_iSlot3 << "|" << std::endl;
-	std::cout << "|" << Wrap(g_iSlot1 + 1) << "|" << Wrap(g_iSlot2 + 1) << "|" << Wrap(g_iSlot3 + 1) << "|" << std::endl;
+	std::cout << "|" << Wrap(g_arriSlot[0] - 1) << "|" << Wrap(g_arriSlot[1] - 1) << "|" << Wrap(g_arriSlot[2] - 1) << "|" << std::endl;
+	std::cout << "|" << g_arriSlot[0] << "|" << g_arriSlot[1] << "|" << g_arriSlot[2] << "|" << std::endl;
+	std::cout << "|" << Wrap(g_arriSlot[0] + 1) << "|" << Wrap(g_arriSlot[1] + 1) << "|" << Wrap(g_arriSlot[2] + 1) << "|" << std::endl;
 }
+
+void RandomizeSlotState()
+{
+	for (int i = 0; i < g_kNUM_SLOTS; i++)
+		g_arriSlot[i] = g_uniR(g_rng);
+}
+
+void Init()
+{
+	g_iPlayerChips = 1000;
+	RandomizeSlotState();
+}
+
+void DisplayMenu()
+{
+	std::cout << "1) Play Slots" << std::endl;
+	std::cout << "2) Quit Slot Machine" << std::endl;
+	std::cout << "3) Credits" << std::endl;
+}
+
+bool ValidateInput(std::string _sInput, std::string _sRegex)
+{
+	std::regex _rRegex(_sRegex);
+	return (std::regex_match(_sInput, _rRegex));
+}
+
+std::string GetValidatedInput(std::string _sRegex)
+{
+	std::string _sInput;
+	while (true)
+	{
+		std::cin >> _sInput;
+		if (std::regex_match(_sInput, std::regex(_sRegex)))
+			return _sInput;
+		else
+			std::cout << "Invalid input" << std::endl;
+	}
+}
+
+void ShowCredits()
+{
+
+}
+
 
 int main()
 {
-	int player_chips = 1000;
+	Init();
 
-	std::random_device rd;     // only used once to initialise (seed) engine
-	std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
-	std::uniform_int_distribution<int> uni(g_kSLOT_MIN, g_kSLOT_MAX); // guaranteed unbiased
+	while (true)
+	{
+		system("cls");
 
-	g_iSlot1 = uni(rng);
-	g_iSlot2 = uni(rng);
-	g_iSlot3 = uni(rng);
+		DrawSlotMachine();
+		std::cout << "Player's chips: " << g_iPlayerChips << std::endl;
+		DisplayMenu();
 
-	DrawSlotMachine();
+		std::string _sMenuSelection = GetValidatedInput("^[123]$");
 
-	int iTemp;
-	std::cin >> iTemp;
+		if (_sMenuSelection == "1")
+		{
+			if (g_iPlayerChips > 0)
+			{
+				std::cout << "Enter amount to bet: ";
+				int _iPlayerBet = std::stoi(GetValidatedInput("[[:digit:]]+"));
+				if (_iPlayerBet > g_iPlayerChips)
+					std::cout << "You don't have enough chips for that" << std::endl;
+				else
+				{
+					g_iPlayerChips -= _iPlayerBet;
+					RandomizeSlotState();
+				}
+			}
+			else
+				std::cout << "You have no chips left to play with" << std::endl;
+		}
+		else if (_sMenuSelection == "2")
+			break;
+		else if (_sMenuSelection == "3")
+			ShowCredits();
+
+		std::this_thread::sleep_for(std::chrono::seconds(2));
+	}
 }
